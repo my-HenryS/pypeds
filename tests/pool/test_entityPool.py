08b39861134d91122542b908f296673207ea0c23
my-HenryS <2994859298@qq.com>
2018-01-29 18:23:00 +0800

    finish test on entity pool

--- tests/pool/test_entityPool.py
+++ tests/pool/test_entityPool.py
@@ -2,13 +2,28 @@ from unittest import TestCase
 
 from pypeds.entity import *
 from pypeds.pool import *
-from pypeds.shape import *
+from pypeds.scene import *
 
 
 class TestEntityPool(TestCase):
     def setUp(self):
-        self.testAgent1 = Agent(Circle2D())
-        self.entities = EntityPool([Agent()])
+        self.agent1 = Agent(1)
+        self.agent2 = Agent(2)
+        self.dummy1 = Dummy(1)
+        self.entities = EntityPool([self.agent1, self.agent2])
 
-    def test_get(self):
-        self.assertEqual()
+    def test_select(self):
+        iter_ = self.entities.select(Agent)
+        self.assertEqual(self.agent1, iter_.__next__())
+        self.assertEqual(self.agent2, iter_.__next__())
+
+    def test_add(self):
+        self.entities.add(self.dummy1)
+        iter_ = self.entities.select(Dummy)
+        self.assertEqual(self.dummy1, iter_.__next__())
+
+    def test_remove(self):
+        self.entities.remove(self.agent1)
+        iter_ = self.entities.select(Agent)
+        self.assertEqual(self.agent2, iter_.__next__())
+
