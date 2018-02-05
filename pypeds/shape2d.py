bcc20e8b01322b3b8ad01347632aba5ce6422dc9
Administrator <xiehn007@sina.com>
2018-02-05 18:32:23 +0800

    Merge the distance calculating of (Circle2D, Point2D) and (Ellipse2D, Point2D)

--- pypeds/shape2d.py
+++ pypeds/shape2d.py
@@ -158,41 +158,10 @@ class DistanceCalculator(object):
             if isinstance(shape, Point2D):
                 dist = other.dist(shape)
                 return dist, ((other.x - shape.x) / dist, (other.y - shape.y) / dist)
-            if isinstance(shape, Circle2D):
+            if isinstance(shape, Circle2D or Ellipse2D):
                 dist, dirt = DistanceCalculator.distance(other, shape)
                 return dist, dirt.mul(-1)
-                # dist, dirt = DistanceCalculator.distance(shape.center, other)
-                # dist -= shape.radius
-                # if dist <= 0:
-                #     return dist, dirt.mul(-1)
-                # return dist, dirt
-            if isinstance(shape, Ellipse2D):
-                dist, dirt = DistanceCalculator.distance(other, shape)
-                return dist, dirt.mul(-1)
-                # l_dist, l_dirt = DistanceCalculator.distance(shape.c_left, other)
-                # r_dist, r_dirt = DistanceCalculator.distance(shape.c_right, other)
-                # m_dist, m_dirt = DistanceCalculator.distance(shape.center, other)
-                # l_dist -= (shape.a - shape.b) / 2
-                # r_dist -= (shape.a - shape.b) / 2
-                # m_dist -= shape.b
-                # if l_dist < r_dist:
-                #     if l_dist < m_dist:
-                #         if l_dist < 0:
-                #             return l_dist, l_dirt.mul(-1)
-                #         return l_dist, l_dirt
-                #     else:
-                #         if m_dist < 0:
-                #             return m_dist, m_dirt.mul(-1)
-                #         return m_dist, m_dirt
-                # else:
-                #     if r_dist < m_dist:
-                #         if r_dist < 0:
-                #             return r_dist, r_dirt.mul(-1)
-                #         return r_dist, r_dirt
-                #     else:
-                #         if m_dist < 0:
-                #             return m_dist, m_dirt.mul(-1)
-                #         return m_dist, m_dirt
+            ## TODO When the shape contains centers of shape and other, dirt calculating maybe is error
             if isinstance(shape, Box2D or Rectangle2D):
                 dist1, dirt1 = DistanceCalculator.distance(shape.get_left, other)
                 dist2, dirt2 = DistanceCalculator.distance(shape.get_right, other)
