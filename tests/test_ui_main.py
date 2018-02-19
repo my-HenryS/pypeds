c3398e4fe2c9d60fa747235c317cd87149e07dc2
hdl <2862308357@qq.com>
2018-02-19 13:56:07 +0800

    update .gitignore

--- tests/test_ui_main.py
+++ tests/test_ui_main.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from pypeds.gui.ui.mainwindow_main import Ui_MainWindow_Main
+from pypeds.gui.ui.mainwindow_main import Ui_MainWindow_Main,Event
+from pypeds.gui.ui.mainwindow_setting import Ui_MainWindow_Setting
 from PyQt5 import QtWidgets
 import sys
 import qdarkstyle
@@ -8,7 +9,9 @@ class TestUI(TestCase):
 
     def test_ui(self):
         app = QtWidgets.QApplication(sys.argv)
-        win = Ui_MainWindow_Main()
         app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-        win.show()
+        ex = Ui_MainWindow_Main()
+        s = Ui_MainWindow_Setting()
+        Event(ex, s)
+        ex.show()
         sys.exit(app.exec_())
