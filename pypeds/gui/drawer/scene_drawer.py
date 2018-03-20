f6953bedd7b3e439fe77fd142c4e255feb0638d3
my-HenryS <2994859298@qq.com>
2018-03-20 17:37:50 +0800

    debug drawer (support register drawer for entities added afterwards)

--- pypeds/gui/drawer/scene_drawer.py
+++ pypeds/gui/drawer/scene_drawer.py
@@ -2,11 +2,16 @@
 
 
 class SceneDrawer(object):
-    def __init__(self, device):
+    def __init__(self, device, register):
         self.device = device
         self.background_color = "black"
+        self.entity_register = register
 
     def draw(self, scene):
         for entity in scene.entities:
-            entity.drawer.draw(entity)
+            if entity.drawer is not None:
+                entity.drawer.draw(entity)
+            else:
+                self.entity_register.add_drawer_support(entity)
+                entity.drawer.draw(entity)
 
