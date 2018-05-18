1a8be5b7c2acf09fe09224cf638044748833ccd2
xiehongnan <32301846+xiehongnan@users.noreply.github.com>
2018-05-18 21:28:25 +0800

    Xiehongnan20180516 (#22)
    * Revise the review
    
    * Merge branch 'master' of https://github.com/my-HenryS/pypeds
    
    # Conflicts:
    #	pypeds/gui/panel.py
    
    * Merge branch 'master' of https://github.com/my-HenryS/pypeds
    
    # Conflicts:
    #	pypeds/example/strategy.py
    
    * Complete def to_dict(self) and def from_dict(self, s_dict)
    
    * Complete def to_dict(self) and def from_dict(self, s_dict)
    
    * 更改场景example.py

--- example.py
+++ example.py
@@ -16,21 +16,49 @@ if __name__ == "__main__":
     scene.model = SFModel(0.004)
     scene.add_listener(PedestrianEscapeListener())
     scene.add_listener(NearestGoalStrategy())
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(3, 4), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(3, 5), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(3, 6), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(3, 7), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(3, 8), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(4, 4), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(4, 5), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(4, 6), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(4, 7), 0.45 / 2, 0.3 / 2, 0)))
-    scene.add_entity(Pedestrian(Ellipse2D(Point2D(4, 8), 0.45 / 2, 0.3 / 2, 0)))
+
+    scene.add_entity(Wall(Box2D(Point2D(20.5, 22.5), 1, 5)))
+    scene.add_entity(Wall(Box2D(Point2D(25.5, 22.5), 1, 5)))
+    scene.add_entity(Wall(Box2D(Point2D(23, 25.5), 6, 1)))
+    scene.add_entity(Wall(Box2D(Point2D(21.75, 20.5), 1.5, 1)))
+    scene.add_entity(Wall(Box2D(Point2D(24.25, 20.5), 1.5, 1)))
+    scene.add_entity(SafetyRegion(Box2D(Point2D(23, 15), 2, 1)))
+
+    scene.add_entity(Pedestrian(Circle2D(Point2D(21.5, 22), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(21.5, 22.5), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(21.5, 23), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(21.5, 23.5), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(21.5, 24), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(21.5, 24.5), 0.45 / 2)))
+
+    scene.add_entity(Pedestrian(Circle2D(Point2D(22.5, 22), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(22.5, 22.5), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(22.5, 23), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(22.5, 23.5), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(22.5, 24), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(22.5, 24.5), 0.45 / 2)))
+
+    scene.add_entity(Pedestrian(Circle2D(Point2D(23.5, 22), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(23.5, 22.5), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(23.5, 23), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(23.5, 23.5), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(23.5, 24), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(23.5, 24.5), 0.45 / 2)))
+
+    scene.add_entity(Pedestrian(Circle2D(Point2D(24.5, 22), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(24.5, 22.5), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(24.5, 23), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(24.5, 23.5), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(24.5, 24), 0.45 / 2)))
+    scene.add_entity(Pedestrian(Circle2D(Point2D(24.5, 24.5), 0.45 / 2)))
+
+
+
     generator = Generator()
     #generator.grid_generate(scene, Box2D(Point2D(5,5),6,6), "Ped", "Circle", 0.5/2, 0, 0, 10, 0, 0, 0, 1)
     #scene.add_entity(SafetyRegion(Box2D(Point2D(10, 30), 1, 4)))
-    scene.add_entity(SafetyRegion(Circle2D(Point2D(10, 30), 4)))
-    scene.to_json("scene1.json")
+
+    #scene.to_json("scene1.json")
 
 
     m.add_scene(scene)
--- pypeds/example/entity.py
+++ pypeds/example/entity.py
@@ -7,7 +7,22 @@ class Pedestrian(Agent):
         self.mass = 80
 
     def to_dict(self):
-        return {"pedestrian": {"shape": self.shape.to_dict(), "velocity":self.velocity, "mass": self.mass}}
+        return {"Pedestrian": {"shape": self.shape.to_dict(), "velocity": self.velocity, "mass": self.mass}}
+
+    def from_dict(self, s_dict):
+        """
+        read from s_dict to generate a Pedestrian
+        :param s_dict:
+        :return:
+        """
+        shape = s_dict["Pedestrian"]["shape"]
+        velocity = s_dict["Pedestrian"]["velocity"]
+        mass = s_dict["Pedestrian"]["mass"]
+        ped = Pedestrian(shape)
+        ped.velocity = velocity
+        ped.mass = mass
+        return ped
+
 
 class Wall(Movable):
     def __init__(self,shape):
@@ -16,8 +31,33 @@ class Wall(Movable):
     def affected(self, affection):
         pass
 
+    def to_dict(self):
+        return {"Wall": {"shape": self.shape.to_dict()}}
+
+    def from_dict(self, s_dict):
+        """
+        read from s_dict to generate a Wall
+        :param s_dict:
+        :return:
+        """
+        shape = s_dict["Wall"]["shape"]
+        return Wall(shape)
+
+
 class SafetyRegion(Goal):
     pass
+
+    def to_dict(self):
+        return {"SafetyRegion": {"shape": self.shape.to_dict()}}
+
+    def from_dict(self, s_dict):
+        """
+        read from s_dict to generate a SafetyRegion
+        :param s_dict:
+        :return:
+        """
+        shape = s_dict["SafetyRegion"]["shape"]
+        return SafetyRegion(shape)
 
 
 class RotatePedestrian(RotateAgent):
@@ -25,3 +65,24 @@ class RotatePedestrian(RotateAgent):
         super(RotatePedestrian, self).__init__(shape)
         self.mass = 80
         self.inertia = 4
+
+    def to_dict(self):
+        return {"RotatePedestrian": {"shape": self.shape.to_dict(), "velocity": self.velocity, "palstance": self.palstance, "inertia": self.inertia, "mass": self.mass}}
+
+    def from_dict(self, s_dict):
+        """
+        read from s_dict to generate a RotatePedestrian
+        :param s_dict:
+        :return:
+        """
+        shape = s_dict["RotatePedestrian"]["shape"]
+        velocity = s_dict["RotatePedestrian"]["velocity"]
+        palstance = s_dict["RotatePedestrian"]["palstance"]
+        inertia = s_dict["RotatePedestrian"]["inertia"]
+        mass = s_dict["RotatePedestrian"]["mass"]
+        ped = RotatePedestrian(shape)
+        ped.palstance = palstance
+        ped.velocity = velocity
+        ped.inertia = inertia
+        ped.mass = mass
+        return ped
